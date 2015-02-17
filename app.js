f1c7cf5cd470c75a511933a0c830ee1f5152b268
BogdanRusinka <rusinka.bogdan@gmail.com>
2015-02-17 11:11:05 +0200

    Fixed images routes

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 angular.module('mdChipsDemo', ['mdChips'])
 	.controller('MainController', function($scope){
 		$scope.itemsCollection = [{
-			thumbnailUrl: '/images/afflek.jpg',
+			thumbnailUrl: './images/afflek.jpg',
 			title: 'Ben Afflek',
 			subtitle: 'ben_a@gmail.com',
-			items: [{subtitle: 'afflek_b@gmail.com', thumbnailUrl: '/images/afflek_2.jpg'}]
+			items: [{subtitle: 'afflek_b@gmail.com', thumbnailUrl: './images/afflek_2.jpg'}]
 		}, {
-			thumbnailUrl: '/images/beil.jpg',
+			thumbnailUrl: './images/beil.jpg',
 			title: 'Christian Beil',
 			subtitle: 'amy@gmail.com',
 			items: [{subtitle: 'beil_christian@gmail.com'}]
 		},{
-			thumbnailUrl: '/images/samuel.jpg',
+			thumbnailUrl: './images/samuel.jpg',
 			title: 'Samuel El Jackson',
 			subtitle: 'samuel@gmail.com',
 			items: []
 		},{
-			thumbnailUrl: '/images/stallone.jpg',
+			thumbnailUrl: './images/stallone.jpg',
 			title: 'Silvester Stallone',
 			subtitle: 'silvester@gmail.com'
 		},{	
 			title: 'Jackie',
-			items: [{subtitle: 'jackie@gmail.com', thumbnailUrl: '/images/jackie.jpg'}]
+			items: [{subtitle: 'jackie@gmail.com', thumbnailUrl: './images/jackie.jpg'}]
 		}];
 
 		$scope.selectedUsers = [];
