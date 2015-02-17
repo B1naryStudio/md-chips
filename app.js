2115a32b6e9239e48e3c2a44c61f188436609194
BogdanRusinka <rusinka.bogdan@gmail.com>
2015-02-17 11:02:31 +0200

    Images for example replaced

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 angular.module('mdChipsDemo', ['mdChips'])
 	.controller('MainController', function($scope){
 		$scope.itemsCollection = [{
-			thumbnailUrl: './afflek.jpg',
+			thumbnailUrl: '../images/afflek.jpg',
 			title: 'Ben Afflek',
 			subtitle: 'ben_a@gmail.com',
-			items: [{subtitle: 'afflek_b@gmail.com', thumbnailUrl: './afflek_2.jpg'}]
+			items: [{subtitle: 'afflek_b@gmail.com', thumbnailUrl: '../images/afflek_2.jpg'}]
 		}, {
-			thumbnailUrl: './beil.jpg',
+			thumbnailUrl: '../images/beil.jpg',
 			title: 'Christian Beil',
 			subtitle: 'amy@gmail.com',
 			items: [{subtitle: 'beil_christian@gmail.com'}]
 		},{
-			thumbnailUrl: './samuel.jpg',
+			thumbnailUrl: '../images/samuel.jpg',
 			title: 'Samuel El Jackson',
 			subtitle: 'samuel@gmail.com',
 			items: []
 		},{
-			thumbnailUrl: './stallone.jpg',
+			thumbnailUrl: '../images/stallone.jpg',
 			title: 'Silvester Stallone',
 			subtitle: 'silvester@gmail.com'
 		},{	
 			title: 'Jackie',
-			items: [{subtitle: 'jackie@gmail.com', thumbnailUrl: './jackie.jpg'}]
+			items: [{subtitle: 'jackie@gmail.com', thumbnailUrl: '../images/jackie.jpg'}]
 		}];
 
 		$scope.selectedUsers = [];
