9a271d8f7960743f973d2a920d83f8e0c8d00e3e
BogdanRusinka <rusinka.bogdann@mail.ru>
2015-03-09 17:33:48 +0200

    Merge with gh-pages

---
+++ app.js
@@ -0,0 +1,34 @@
+angular.module('mdChipsDemo', ['mdChips'])
+	.controller('MainController', function($scope){
+		$scope.itemsCollection = [{
+			thumbnailUrl: 'images/afflek.jpg',
+			title: 'Ben Afflek',
+			subtitle: 'ben_a@gmail.com',
+			items: [{subtitle: 'afflek_b@gmail.com', thumbnailUrl: 'images/afflek_2.jpg'}]
+		}, {
+			thumbnailUrl: 'images/beil.jpg',
+			title: 'Christian Beil',
+			subtitle: 'christian_b@gmail.com',
+			items: [{subtitle: 'beil_christian@gmail.com'}]
+		},{
+			thumbnailUrl: 'images/samuel.jpg',
+			title: 'Samuel El Jackson',
+			subtitle: 'samuel@gmail.com',
+			items: []
+		},{
+			thumbnailUrl: 'images/stallone.jpg',
+			title: 'Silvester Stallone',
+			subtitle: 'silvester@gmail.com'
+		},{	
+			title: 'Jackie Chan',
+			items: [{subtitle: 'jackie@gmail.com', thumbnailUrl: 'images/jackie.jpg'}]
+		},{
+			title: 'Angelina Jolie',
+			subtitle: 'jolie@gmail.com'
+		},{
+			title: 'Contact with a veru long name to show',
+			subtitle: 'contactwithverylongemail@gmail.com really'
+		}];
+
+		$scope.selectedUsers = [];
+	});
